7c75aaf360146267b494664c5a35eda169aefecb
connorpmartin <55263157+connorpmartin@users.noreply.github.com>
2021-02-25 09:25:59 -0500

    updated data to hourwise

--- src/data_gen.jl
+++ src/data_gen.jl
@@ -14,8 +14,8 @@ days = Dict([
 ])
 
 n = 100
-data = Array{Any}(missing,n,4+96*7)
-autolabels = Array{String}(undef,96*7)
+data = Array{Any}(missing,n,4+24*7)
+autolabels = Array{String}(undef,24*7)
 #number of people we're generating
 for i in 1:n
     data[i,1] = randstring(rand(4:8))
@@ -27,12 +27,13 @@ for i in 1:n
     for j in 0:6
         local d = Normal(0,1 + 2*rand())
         local lo,hi = -2,2
-        local x = range(lo,hi;length=96)
-        data[i,96*j+5:96*j+100] = pdf.(d,x) * (.75+.5 * rand()) / mean(pdf.(d,x)) #up to double the dist
+        local x = range(lo,hi;length=24)
+        data[i,24*j+5:24*j+28] = pdf.(d,x) * (.75+.5 * rand()) / mean(pdf.(d,x)) #up to double the dist
     end
 end
+
 for j in 0:6
-    autolabels[96*j+1:96*j+96] = string.(days[j],string.(1:96))
+    autolabels[24*j+1:24*j+24] = string.(days[j],string.(1:24))
 end
 
 #generate the label scheme
@@ -47,3 +48,8 @@ CSV.write("sample_data.csv", df)
 #standard congestion at a data point is 1.
 #average congestion per day varies from .75 to 1.25.
 #might wanna add more customization if you want?
+plot()
+plot(title = "Monday at 01",data[1,5:28],yrange=[.5,1.5],label = "Monday at 01",xlabel = "Hour",ylabel = "Congestion")
+plot!(title = "Tuesday at 01",data[1,29:52],yrange=[.5,1.5])
+plot!(title = "Monday at 02",data[2,5:28],yrange=[.5,1.5],label = "Monday at 02")
+plot!(title = "Data",data[1,29:52],yrange=[.5,1.5],label = "Tuesday at 02")
