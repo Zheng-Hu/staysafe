9cdcb8c72ed8bd3e16089fd694733dc2de676352
Qinjuan Xie <qinjuanx@umich.edu>
2021-03-11 00:11:29 -0500

    Update __init__.py

--- staysafe/views/__init__.py
+++ staysafe/views/__init__.py
@@ -1,14 +1,4 @@
 """Views, one for each StaySafe page."""
 from staysafe.views.index import show_index
-from staysafe.views.user import show_user
-from staysafe.views.followers import show_follower
-from staysafe.views.following import show_following
-from staysafe.views.post import show_post
-from staysafe.views.explore import show_explore
-from staysafe.views.login import show_login
-from staysafe.views.logout import show_logout
-from staysafe.views.create import show_create
-from staysafe.views.delete import show_delete
-from staysafe.views.edit import show_edit
-from staysafe.views.password import show_password
-from staysafe.views.uploads import show_upload
+from staysafe.views.index import show_Duder
+from staysafe.views.index import show_Predict
