fec9b20bc6d235e2fc204cc6d2e6a27261c3a371
shirleydongj <35925336+shirleydongj@users.noreply.github.com>
2021-03-31 14:55:09 +0800

    Update __init__.py

--- staysafe/views/__init__.py
+++ staysafe/views/__init__.py
@@ -2,4 +2,4 @@
 from staysafe.views.index import show_index
 from staysafe.views.predict import show_prediction
 from staysafe.views.index import show_Duder
-from staysafe.views.index import show_Predict
+#from staysafe.views.index import show_Predict
