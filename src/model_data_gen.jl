b1f2d51bbc29b1b484dac22ce60bcbdfe6990ac8
connorpmartin <55263157+connorpmartin@users.noreply.github.com>
2021-04-08 10:37:49 -0400

    basic datagen implementation basic model structure

--- src/model_data_gen.jl
+++ src/model_data_gen.jl
@@ -1,8 +1,36 @@
 
-#n by 24 * 7 * (num_days) + 10 (for some set of potential labels)
+
+
+#n by 24 by 7 by (num_days), with a separate  
+#the use of multiple labels averages models
 function model_data_gen(n = 100,num_days = 10)
-    ret = Array{Float64}(undef,n,24*7*num_days+10)
+    ret = Array{Array{Float32}}(undef,n)
+    real = Array{Array{Float32}}(undef,n)
+    #splitting into 3D assumes datapoint relationships based on proximity
+    #i think this is reasonable, as x-proximity is hour proximity,
+    #and y-proximity is also similar based on time.
+    #z-proximity is relevant if we assume that the days are in chronological order
+    
     for i in 1:n
+        reti = Array{Float32}(24,7,num_days)
+        reali = Array{Float32}(24,7)
+        #TOOO: entangle across days and hrs in starting init
+        #need proximity based bleed 
+        #(assumed trends over time -> similarity based on close timescale)
+        reali = randn(24,7) 
         for j in 1:num_days
+            #entangle across samples
+            #each sample has some arbitrary change
+            #TODO: proximity based!!
+            reti[:,:,j] = reali .+ randn()
+        end
+        #add small noise
+        reti .+= .25*randn(size(reti))
+        #make all the congestions between 0 and 1
+        reti .= exp.(reti .- maximum(reti))
+        #set
+        ret[i] = reti
+        real[i] = reali
     end
+    return ret,real
 end
--- src/prediction.jl
+++ src/prediction.jl
@@ -1,1 +1,45 @@
+using Flux: ADAM, gradient, glorot_uniform, conv, train!, Chain, params, mse
+using Flux.Optimise: update!
 #trains and utilizes a prediction model to output congestion metrics
+function predict(filter_size)
+
+
+    data,real = model_data_gen()
+    #split into train and valid
+
+
+    #using 3D conv models because they're cool and seem relevant
+    #performing learned calcs with bleed into other days of week
+    cfilter1 = glorot_normal(3,3,3,1,9) #3645 params. maybe decrease channel size
+    cfilter2 = glorot_normal(3,3,3,9,81) #consider decreasing channels
+    conv1 = x -> conv(x,cfilter1,pad=1)
+    conv2 = x -> conv(x,cfilter2,pad=1)
+    
+    #the output of the conv2 layer will be a 5D tensor, 24 x 7 x (num days) x 1 x 81. We need to get that into something workable.
+    #and we need to simplify numdays x 81 to a single congestion metric.
+    #todo: combine
+    combine = glorot_normal(64,16)
+
+
+    #
+    model = Chain(conv1,x -> relu.(x),conv2,x -> relu.(x),
+                    x -> mapslices(y -> fc1 * y,x,dims=[3]))
+
+    #note that this model is more denoising than prediction...
+    #i am effectively "denoising" a 24/7 set of data to get hopefully
+    #accurate prediction. not sure if this is the right approach. 
+    
+
+    loss = 
+
+    for i in 1:niters
+        #train the model on our data
+    end
+
+
+    #grade the model on data after each iterations
+
+
+    #TODO: run on test and output
+
+end
