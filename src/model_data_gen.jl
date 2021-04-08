aea92319ef5a7f21fa1ad49d75f653c3b181b6ad
connorpmartin <55263157+connorpmartin@users.noreply.github.com>
2021-04-08 09:12:21 -0400

    Update model_data_gen.jl

---
+++ src/model_data_gen.jl
@@ -0,0 +1,8 @@
+
+#n by 24 * 7 * (num_days) + 10 (for some set of potential labels)
+function model_data_gen(n = 100,num_days = 10)
+    ret = Array{Float64}(undef,n,24*7*num_days+10)
+    for i in 1:n
+        for j in 1:num_days
+    end
+end
